0bb9a4c492842dc3a8f524481476bf9f027adf9d
Егор <egor.lavrinovich.00@gmail.com>
2025-05-25 12:57:02 +0300

    Зачтено

---
+++ Lab_14/v1/v1.cpp
@@ -0,0 +1,182 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <Windows.h>
+#include <chrono>
+
+using namespace std;
+
+const int TABLE_SIZE = 16;
+const int PRIME = 1000003; 
+
+class HashNode {
+public:
+    int year;
+    string product;
+    HashNode* next;
+
+    HashNode(int y, const string& p) : year(y), product(p), next(nullptr) {}
+};
+
+class HashTable {
+private:
+    vector<HashNode*> table;
+    int a, b, p, size;
+
+    int hashFunc(int key) {
+        return ((a * key + b) % p) % size;
+    }
+
+public:
+    HashTable(int s = TABLE_SIZE, int prime = PRIME) : size(s), p(prime) {
+        table.resize(size, nullptr);
+        srand(time(0));
+        a = 1 + rand() % (p - 1); 
+        b = rand() % p;           
+    }
+
+    ~HashTable() {
+        for (auto& head : table) {
+            while (head) {
+                HashNode* temp = head;
+                head = head->next;
+                delete temp;
+            }
+        }
+    }
+
+    void insert(int year, const string& product) {
+        int index = hashFunc(year);
+        HashNode* newNode = new HashNode(year, product);
+        newNode->next = table[index];
+        table[index] = newNode;
+    }
+
+    int search(int year, const string& product) {
+        int index = hashFunc(year);
+        HashNode* cur = table[index];
+        while (cur) {
+            if (cur->year == year)
+            cur = cur->next;
+        }
+        return 1;
+    }
+
+    bool remove(int year, const string& product) {
+        int index = hashFunc(year);
+        HashNode* cur = table[index];
+        HashNode* prev = nullptr;
+
+        while (cur) {
+            if (cur->year == year) {
+                if (prev) prev->next = cur->next;
+                else table[index] = cur->next;
+                delete cur;
+                return true;
+            }
+            prev = cur;
+            cur = cur->next;
+        }
+        return false;
+    }
+
+    void print() {
+        cout << "\nХеш-таблица:\n";
+        for (int i = 0; i < size; ++i) {
+            HashNode* cur = table[i];
+            if (cur) {
+                cout << "[" << i << "]: ";
+                while (cur) {
+                    cout << "(" << cur->product << ", " << cur->year << ")";
+                    cur = cur->next;
+                }
+                cout << endl;
+            }
+        }
+    }
+};
+
+void menu() {
+    cout << "\n(1) Добавить товар\n"
+        << "(2) Показать таблицу\n"
+        << "(3) Удалить товар\n"
+        << "(4) Поиск товара\n"
+        << "(5) Выход\n"
+        << "> Выбор: ";
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "RU");
+
+    HashTable ht;
+    int choice;
+
+    while (true) {
+        menu();
+        cin >> choice;
+        cin.ignore();
+
+        switch (choice) {
+        case 1: {
+            string name;
+            int year;
+            cout << "Введите название товара: ";
+            getline(cin, name);
+            cout << "Введите год выпуска: ";
+            cin >> year;
+            ht.insert(year, name);
+            cout << "Товар добавлен.\n";
+            break;
+        }
+        case 2:
+            ht.print();
+            break;
+        case 3: {
+            string name;
+            int year;
+           
+            cout << "Введите год выпуска: ";
+            cin >> year;
+            if (ht.remove(year, name))
+                cout << "Товар удалён.\n";
+            else
+                cout << "Товар не найден.\n";
+            break;
+        }
+        case 4: {
+            string name;
+            int year;
+            cout << "Введите год выпуска: ";
+            cin >> year;
+
+            auto start = chrono::high_resolution_clock::now();
+            int result = ht.search(year, name);
+            auto end = chrono::high_resolution_clock::now();
+            chrono::duration<double, milli> time = end - start;
+
+            if (result)
+                cout << "Найден товар: " << result << "\n";
+            else
+                cout << "Товар не найден.\n";
+
+            cout << "Время поиска: " << time.count() << " мс\n";
+            break;
+        }
+        case 5:
+            cout << "Выход...\n";
+            return 0;
+        default:
+            cout << "Некорректный ввод. Повторите.\n";
+            break;
+        }
+
+        system("pause");
+        system("cls");
+    }
+
+    return 0;
+}
---
+++ Lab_14/v13/v13.cpp
@@ -0,0 +1,200 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+
+const int MAXSIZE = 16;
+
+class hashNode {
+public:
+    int key;
+    string city;
+    hashNode* next;
+
+    hashNode(int k, const string& c) : key(k), city(c), next(nullptr) {}
+};
+
+class hashTable {
+private:
+    vector<hashNode*> table;
+    int size;
+
+
+    int hashFunc(const string& city) {
+        int hashVal = 0;
+        for (char ch : city) {
+            hashVal ^= static_cast<unsigned char>(ch);
+        }
+        return hashVal % size;
+    }
+
+public:
+    hashTable(int sz = MAXSIZE) : size(sz) {
+        table.resize(size, nullptr);
+    }
+
+    ~hashTable() {
+        for (int i = 0; i < size; ++i) {
+            hashNode* cur = table[i];
+            while (cur != nullptr) {
+                hashNode* tmp = cur;
+                cur = cur->next;
+                delete tmp;
+            }
+        }
+    }
+
+    void insert(int key, const string& city) {
+        int index = hashFunc(city);
+        hashNode* cur = table[index];
+
+        while (cur != nullptr) {
+            if (cur->key == key && cur->city == city) {
+                cur->city = city; 
+                return;
+            }
+            cur = cur->next;
+        }
+
+        
+        hashNode* newNode = new hashNode(key, city);
+        newNode->next = table[index];
+        table[index] = newNode;
+    }
+
+    int search(int key, const string& city) {
+        int index = hashFunc(city);
+        hashNode* cur = table[index];
+        while (cur != nullptr) {
+            if (cur->key == key ) {
+                break;
+            }
+            cur = cur->next;
+        }
+        return 1;
+    }
+
+    bool remove(int key, const string& city) {
+        int index = hashFunc(city);
+        hashNode* cur = table[index];
+        hashNode* prev = nullptr;
+
+        while (cur) {
+            if (cur->key == key) {
+                if (prev) prev->next = cur->next;
+                else table[index] = cur->next;
+                delete cur;
+                return true;
+            }
+            prev = cur;
+            cur = cur->next;
+        }
+        return false;
+    }
+
+    void print() {
+        cout << "\nХеш-таблица:\n";
+        for (int i = 0; i < size; ++i) {
+            hashNode* cur = table[i];
+            if (cur) {
+                cout << "[" << i << "]: ";
+                while (cur != nullptr) {
+                    cout << "(" << cur->city << ", " << cur->key << ")";
+                    cur = cur->next;
+                }
+                cout << endl;
+            }
+        }
+        cout << endl;
+    }
+};
+
+
+void menu() {
+    cout << "\n(1) Ввод\n"
+        << "(2) Вывод всей таблицы\n"
+        << "(3) Удалить\n"
+        << "(4) Поиск\n"
+        << "(5) Выход\n"
+        << "> Выбор: ";
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    hashTable ht;
+    int sw;
+
+    while (true) {
+        menu();
+        cin >> sw;
+        cin.ignore(); 
+
+        switch (sw) {
+        case 1: {
+            string city;
+            int population;
+            cout << "Введите кол. студентов: ";
+            getline(cin, city);
+            cout << "Введите номер группы: ";
+            cin >> population;
+            ht.insert(population, city);
+            cout << "Группа добавлена добавлен.\n";
+            break;
+        }
+        case 2:
+            ht.print();
+            break;
+        case 3: {
+            string city;
+            int population;
+            
+            cout << "Введите номер группы: ";
+            cin >> population;
+            if (!ht.remove(population, city)) {
+                cout << "Удалён.\n";
+            }
+            else {
+                cout << "Не найден.\n";
+            }
+            break;
+        }
+        case 4: {
+            string city;
+            int population;
+           
+            cout << "Введите номер группы: ";
+            cin >> population;
+
+            auto start = chrono::high_resolution_clock::now();
+            bool result = ht.search(population, city);
+            auto end = chrono::high_resolution_clock::now();
+            chrono::duration<double, milli> duration = end - start;
+
+            if (result) {
+                cout << "Найден: " << result << "\n";
+            }
+            else {
+                cout << "Не найден.\n";
+            }
+            cout << "Время поиска: " << duration.count() << " мс\n";
+            break;
+        }
+        case 5:
+            cout << "Выход...\n";
+            return 0;
+        default:
+            cout << "Неверный ввод. Попробуйте снова.\n";
+            break;
+        }
+        system("pause");
+        system("cls");
+    }
+
+    return 0;
+}
